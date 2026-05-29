f88077e8b489f974a587225ce6a32a393a5af901
huyquang-oss <huyquang22072006@gmail.com>
2026-05-29 14:42:30 +0700

    implement time-series cross-validation

--- src/model/Logistic_Regression/log_reg_model.py
+++ src/model/Logistic_Regression/log_reg_model.py
@@ -1,9 +1,10 @@
 from sklearn.linear_model import LogisticRegression
-def build_model():
-    model = LogisticRegression(max_iter=1000)
+def build_model(**kwargs):
     param_grid = {
-        'C': [0.01, 0.1, 1, 10, 100],
-        'solver': ['liblinear', 'lbfgs']
+        'C': kwargs.get('C', 1.0),
+        'solver': kwargs.get('solver', 'lbfgs'),
+        'max_iter': kwargs.get('max_iter', 1000),
+        'random_state': kwargs.get('random_state', 42)
     }
     
-    return model, param_grid
+    return LogisticRegression(**param_grid)
